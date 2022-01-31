16a0607cadf46238b77e140ddc8f685a8764f561
Thomas.Schumacher <Tommyschumacher@gmail.com>
2022-01-30 19:16:03 -0600

    Completes functionality

--- WaM.js
+++ WaM.js
@@ -1,43 +1,51 @@
 let timer = 0 
 let gameActive = false
-document.getElementById("startTheGame").addEventListener("click", function (){
-    time = 30
-    gameActive = true
+let score = 0
+document
+    .getElementById("startTheGame")
+    .addEventListener("click", function (){
+        time = 30
+        gameActive = true
     
-document.getElementById("startTheGame").style.display = "none"
+        document.getElementById("startTheGame").style.display = "none"
 
-    let timerInterval = setInterval(() => {
-        if(time > 0){
-        time--
-    }
-    else {
-        gameActive = false
-        clearInterval(timerInterval)
-    }
+        let timerInterval = setInterval(() => {
+            if(time > 0){
+                time--
+            } else {
+                gameActive = false
+                clearInterval(timerInterval)
+            }
 
-        console.log(time);
-        document.getElementById("Timer").innerText = `Time ${time}`
-    }, 1000);
-    let moleInterval = setInterval(() => {
-       if(!gameActive){
-           clearInterval(moleInterval)
-           return
-       } 
-    let moles = document.querySelectorAll(".moleHoles")
+            console.log(time);
+            document.getElementById("Timer").innerText = `Time ${time}`
+        }, 1000)
 
-    let moleActive = Math.floor(Math.random() * moles.length)
-    moles[moleActive].classList.add("moles")
+        let moleInterval = setInterval(() => {
+            if(!gameActive){
+                clearInterval(moleInterval)
+                return
+            } 
+            let moles = document.querySelectorAll(".moleHoles")
 
-    setTimeout(() => {
-        console.log(moleActive);
-        moles[moleActive].classList.remove("moles")
-    }, 750)
+            let moleActive = Math.floor(Math.random() * moles.length)
+            moles[moleActive].classList.add("moles")
+            let addScore = function(event){ 
+                score++
+                document.getElementById("Score").innerText = `SCORE: ${score}`
+
+            };
+            moles[moleActive].addEventListener("click", addScore)
     
 
-}, 500);
+            setTimeout(() => {
+                console.log(moleActive);
+                moles[moleActive].classList.remove("moles")
+                moles[moleActive].removeEventListener("click", addScore)
+    
+            }, 750)
 
-document.getElementsByClassName("moleHoles").forEach((element) => {
-    element.addEventListener('click')
-})
+            
+        }, 500);
 
-)}
+    })
